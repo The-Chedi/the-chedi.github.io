92cb5687f842beb8ccff907b27cc647a385f2c1d
The-Chedi <textgame91@gmail.com>
2026-05-24 14:31:22 -0500

    Update Firebase configuration and app ID

--- kitchen-system-order/firebase-config.js
+++ kitchen-system-order/firebase-config.js
@@ -1,17 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD57ErKNYwqGbLcriDYYvPUtVIw8jRncXQ",
   authDomain: "system-order-kitchen.firebaseapp.com",
-  databaseURL: "https://system-order-kitchen-default-rtdb.firebaseio.com",
   projectId: "system-order-kitchen",
   storageBucket: "system-order-kitchen.firebasestorage.app",
   messagingSenderId: "429224554052",
-  appId: "1:429224554052:web:7536c4cdea334679fcd611"
+  appId: "1:429224554052:web:85d32727439fe55efcd611"
 };
 
 // Initialize Firebase
