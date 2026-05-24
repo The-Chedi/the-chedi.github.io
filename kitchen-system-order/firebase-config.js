1b7052d7220e3fb187e483434822b4abfd047417
The-Chedi <textgame91@gmail.com>
2026-05-24 14:41:32 -0500

    Refactor Firebase initialization logic
    Updated Firebase initialization to check for existing app instance before initializing.

--- kitchen-system-order/firebase-config.js
+++ kitchen-system-order/firebase-config.js
@@ -1,11 +1,18 @@
+// firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyD57ErKNYwqGbLcriDYYvPUtVIw8jRncXQ",
-  authDomain: "system-order-kitchen.firebaseapp.com",
-  projectId: "system-order-kitchen",
-  storageBucket: "system-order-kitchen.firebasestorage.app",
-  messagingSenderId: "429224554052",
-  appId: "1:429224554052:web:85d32727439fe55efcd611"
+    apiKey: "AIzaSyD57ErKNYwqGbLcriDYYvPUtVIw8jRncXQ",
+    authDomain: "system-order-kitchen.firebaseapp.com",
+    projectId: "system-order-kitchen",
+    storageBucket: "system-order-kitchen.firebasestorage.app",
+    messagingSenderId: "429224554052",
+    appId: "1:429224554052:web:85d32727439fe55efcd611"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+
+console.log("✅ Firebase initialized successfully");
